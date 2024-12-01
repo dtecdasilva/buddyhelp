bbf4afff3a1ee44d55c3926851d8cd417886e517
unknown <dtecdasilva@gmaile.com>
2024-12-01 23:50:50 +0100

    Initial committ

--- components/About.tsx
+++ components/About.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, CardHeader, CardTitle, CardContent, CardDescription } from "@/components/ui/card";
+import { Card, CardHeader, CardTitle, CardDescription } from "@/components/ui/card";
 import Image from "next/image";
 import { Check } from "lucide-react";
 
@@ -31,7 +31,7 @@ function About() {
         <div className="w-full md:w-1/2 animate-slide-in-right">
           <p className="text-gray-700"></p>
           At Buddy Help, we understand the importance of mental health and the power of human connection. Our mission is to make emotional support accessible to everyone, anytime, and anywhere. Whether you're looking for someone to talk to or just need a quick chat to feel heard, Buddy Help is here to be your supportive companion.
-          Join our community today, and let’s work together towards better mental well-being!
+          Join our community today, and let us work together towards better mental well-being!
             <div className="space-y-4 p-6 rounded-lg max-w-md">
               {services.map((service, index) => (
                 <div key={index} className="flex items-center space-x-2">
